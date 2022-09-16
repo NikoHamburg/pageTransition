ff39070fcf6170e9a56b04f4902335418596c89c
NikoHamburg <nikotrettin@googlemail.com>
2022-09-16 12:27:02 +0200

    single player with css transition

--- src/pages/Player.js
+++ src/pages/Player.js
@@ -9,43 +9,43 @@ class Player extends Lightning.Component {
     //   "http://vf-maf-lng.s3.eu-central-1.amazonaws.com/LNG_MVP_Big_Bunny_AWS_Test.mp4"
     // );
     const video1 = document.createElement("video");
-    const video2 = document.createElement("video");
+    // const video2 = document.createElement("video");
     // const div1 = document.createElement('div');
     // div1.height = 500;
     // div1.width = 
 
-    video1.width = 960;
-    video1.height = 540;
-    video1.left = 0;
-    video1.top = 0;
+    video1.width = 1720;
+    video1.height = 880;
+    video1.left = 100;
+    video1.top = 100;
     video1.setAttribute(
       "src",
       "http://vf-maf-lng.s3.eu-central-1.amazonaws.com/LNG_MVP_Big_Bunny_AWS_Test.mp4"
     );
-    video1.setAttribute('controls', 'autoplay');
+    // video1.setAttribute('controls', 'autoplay');
     // video1.controls = true;
     // video1.autoplay = true;
     document.body.appendChild(video1);
     // video1.play();
 
-    video2.width = 960;
-    video2.height = 540;
-    video2.left = 960;
-    video2.setAttribute(
-      "src",
-      "http://vf-maf-lng.s3.eu-central-1.amazonaws.com/video-30sec.mp4"
-    );
-    video2.setAttribute('controls', 'autoplay');
-    document.body.appendChild(video2);
+    // video2.width = 960;
+    // video2.height = 540;
+    // video2.left = 960;
+    // video2.setAttribute(
+    //   "src",
+    //   "http://vf-maf-lng.s3.eu-central-1.amazonaws.com/video-30sec.mp4"
+    // );
+    // video2.setAttribute('controls', 'autoplay');
+    // document.body.appendChild(video2);
   }
 
   _handleRight() {
     const video1 = document.getElementsByTagName("video")[0];
     video1.style.transition = `all 0.5s ease-out`;
     video1.style.transform = "translate(-1920px)";
-    const video2 = document.getElementsByTagName("video")[1];
-    video2.style.transition = `all 0.5s ease-out`;
-    video2.style.transform = "translate(-1920px)";
+    // const video2 = document.getElementsByTagName("video")[1];
+    // video2.style.transition = `all 0.5s ease-out`;
+    // video2.style.transform = "translate(-1920px)";
     // Router.navigate("Player2");
   }
 
@@ -53,9 +53,9 @@ class Player extends Lightning.Component {
     const video1 = document.getElementsByTagName("video")[0];
     video1.style.transition = `all 0.5s ease-out`;
     video1.style.transform = `translate(0)`;
-    const video2 = document.getElementsByTagName("video")[1];
-    video2.style.transition = `all 0.5s ease-out`;
-    video2.style.transform = `translate(0)`;
+    // const video2 = document.getElementsByTagName("video")[1];
+    // video2.style.transition = `all 0.5s ease-out`;
+    // video2.style.transform = `translate(0)`;
   }
   //   pageTransition() {
   //     return "left";
